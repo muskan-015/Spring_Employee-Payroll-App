ac7f371263146a7aa006fb952059355ba5d7d6c2
muskan-015 <muskanvds2004@gmail.com>
2025-02-27 16:58:48 +0530

    [Muskan]Updating UC14

--- EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/exceptionhandler/GlobalExceptionHandler.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/exceptionhandler/GlobalExceptionHandler.java
@@ -1,12 +1,16 @@
 package com.bridgelabz.employeepayrollapp.exceptionhandler;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 //custom exception handler class
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -20,5 +24,12 @@ public class GlobalExceptionHandler {
 
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<String> handleInvalidJsonException(HttpMessageNotReadableException ex) {
+        return ResponseEntity.badRequest().body("Invalid request format. Please check your input JSON.");
+    }
+
+
 }
 
